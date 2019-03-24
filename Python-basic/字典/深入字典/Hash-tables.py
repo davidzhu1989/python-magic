d9646800ab6e4a30de893a2303ee9575cee09168
davidzhu <hideonc@outlook.com>
2019-03-24 21:06:32 +0800

    字典的一些乱七八糟

---
+++ Python-basic/字典/深入字典/Hash-tables.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# @Time : 2019/3/24 13:42 
+__author__ = 'Boaz'
+
+print(map(hash,(0,1,2,3)))
---
+++ Python-basic/字典/深入字典/demo0.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# @Time : 2019/3/24 13:40 
+__author__ = 'Boaz'
+
+# 首先添加三个键/值
+
+d = {'a':1, 'b':2}
+d['c'] = 3
+print(d)
+print(d['a'])
+print(d['b'])
+print(d['c'])
+print(d['d'])   # error
