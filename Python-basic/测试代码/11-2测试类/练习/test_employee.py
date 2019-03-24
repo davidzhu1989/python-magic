f2fe50728424852618858607cc8126e0604a6fb7
davidzhu <hideonc@outlook.com>
2019-03-24 21:15:30 +0800

    修改了一下，因为前面的myemployee也修改了。

--- Python-basic/测试代码/11-2测试类/练习/test_employee.py
+++ Python-basic/测试代码/11-2测试类/练习/test_employee.py
@@ -9,10 +9,13 @@ from myemployee import Employee
 class TestEmployee(unittest.TestCase):
     def setUp(self):
         self.employee = Employee('Scott', 'Joplin',4000)
-        # self.employee.give_raise()
 
     def test_give_default_raise(self):
-        self.assertEqual(9000, self.employee.give_raise())
+        self.employee.give_raise()
+        self.assertEquals(self.employee.salary, 9000)
+        # self.assertEqual(9000, self.employee.give_raise())
 
     def test_give_custom_raise(self):
-        self.assertEqual(10000, self.employee.give_raise(1000))
+        self.employee.give_raise(1000)
+        self.assertEqual(self.employee.salary, 5000)
+        # self.assertEqual(10000, self.employee.give_raise(1000))
